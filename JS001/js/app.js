303e240541724c5d9cb485b216465f20d69011ed
Nickolas Andre <nickolas.sam3@gmail.com>
2023-03-24 20:06:18 -0300

    Update app.js

--- JS001/js/app.js
+++ JS001/js/app.js
@@ -1,1 +1,34 @@
 console.log("Olá Mundo");
+
+//let
+//var
+//const = é uma constante, uma variável que não muda o valor ao longo código.
+
+const nome1 = "andre"; 
+
+//let nome = "nickolas";
+
+var nome2 = "andre2";
+
+{
+    var nome2 = "andre3";
+    console.log(nome2);
+}
+
+console.log(nome2);
+
+
+let nome = "nickolas"; // string
+let sobrenome = "andre";
+let string = `O ${nome} tem o sobrenome ${sobrenome}`;
+let idade = 25; //number
+let feliz = true; //boolean
+let nada; //undefined
+let simbolo = Symbol();
+let object = {} //objeto
+
+
+
+console.log(nome + ' ' + sobrenome)
+
+console.log(string)

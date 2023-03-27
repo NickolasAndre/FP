803fadc1d9623835969c3b8af1751ef8bf08c415
Nickolas Andre <nickolas.sam3@gmail.com>
2023-03-27 19:12:46 -0300

    commit

---
+++ JS002/js/app.js
@@ -0,0 +1,43 @@
+/*let condicao2 = 1;
+
+
+if(variavel){
+    console.log('É verdadeiro');
+}
+else{
+    console.log('É falso');
+}
+
+
+if(condicao1){
+    console.log('A primeira condição foi satisfeita!');
+}
+else if(condicao2){
+    console.log('A segunda condição foi satisfeita!');
+}
+else if(condicao3){
+    console.log('A terceira condição foi satisfeita!');
+
+}else{
+    console.log('Nenhuma condição foi satisfeita!');
+}
+*/
+
+let nome = 'andre';
+
+switch(nome){
+    case 'andre':
+        console.log('esse é o sobrenome');
+    break;
+
+    case 'nickolas':
+        console.log('esse é o nome');
+    break;
+
+    case 'siqueira':
+        console.log('esse é o terceiro nome');
+    break;
+
+    default:
+        console.log('essa é a exceção');
+}

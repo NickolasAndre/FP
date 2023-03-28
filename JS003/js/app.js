1d133bf5454513acf5611424c5b5d6239f7ed109
Nickolas Andre <nickolas.sam3@gmail.com>
2023-03-28 19:28:38 -0300

    commit

---
+++ JS003/js/app.js
@@ -0,0 +1,29 @@
+/*function areaRetangulo(a, b){
+    return a * b;
+}
+
+function HelloWorld(){
+    console.log('Helloworld no console')
+}
+
+HelloWorld();
+*/
+
+/*function areaRetangulo(a, b){
+    return a * b
+}
+
+console.log(areaRetangulo(4, 2));
+*/
+
+/*const areaRetangulo = (a, b) => a * b;
+
+console.log(areaRetangulo(6, 2))
+*/
+
+
+const upperName = function(name){
+    return name.toUpperCase();
+}
+
+console.log(upperName('nickolas'))

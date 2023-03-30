7c3c42bf2de4a661e216566c99c4dcc7ff8e34f7
Nickolas Andre <nickolas.sam3@gmail.com>
2023-03-30 19:18:45 -0300

    commit

---
+++ JS005/js/app.js
@@ -0,0 +1,14 @@
+/*let frutas = ['maçã', 'uva', 'banana', 'laranja'];
+
+console.log(frutas[2]);
+*/
+
+let frutas = ['maçã', 'uva', 'mamão', 'laranja', ['nanica', 'prata']];
+
+//console.log(frutas[4][1]);
+
+console.log(frutas.pop());
+console.log(frutas.push('kiwi'));
+console.log(frutas);
+console.log(frutas.length);
+

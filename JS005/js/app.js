fde8c83641591d1d4769a632363435a4868d73c5
Nickolas Andre <nickolas.sam3@gmail.com>
2023-03-30 21:56:02 -0300

    Update app.js

--- JS005/js/app.js
+++ JS005/js/app.js
@@ -3,7 +3,8 @@
 console.log(frutas[2]);
 */
 
-let frutas = ['maçã', 'uva', 'mamão', 'laranja', ['nanica', 'prata']];
+let frutas = ['maçã', 'uva', 'mamão', 'laranja', 'kiwi']; 
+//let frutas = ['maçã', 'uva', 'mamão', 'laranja', ['nanica', 'prata']];
 
 //console.log(frutas[4][1]);
 
@@ -12,3 +13,32 @@ console.log(frutas.push('kiwi'));
 console.log(frutas);
 console.log(frutas.length);
 
+/*
+for(let i = 1; i <= 12; i++){ //i++ => i = i + 1
+    console.log(i);
+} 
+*/
+
+
+for(let i = 0; i < frutas.length; i++){
+    console.log(frutas[i]);
+}
+
+
+/*let i = 1;
+
+while(i <= 12){
+
+    console.log(i);
+
+    i++;
+}*/
+
+/*
+let i = 0;
+
+while(i <= 4){
+    console.log(i + ': ' + frutas[i]);
+    i++;
+}
+*/

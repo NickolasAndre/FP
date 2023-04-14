fb732e6407561ce9f8d3438e9e47c37969f04222
Nickolas Andre <nickolas.sam3@gmail.com>
2023-04-13 21:05:47 -0300

    commit

---
+++ JS016-jason/js/app.js
@@ -0,0 +1,18 @@
+//JSON.parse(); // transforma o texto JSOM em objeto
+
+//JSON. stringify(); // transformar um objeto do JavaScript em JSON
+
+
+const json = JSON.parse('{"id": 1,"nome": "Notebook Dell","modelo": "Gamer","quantidade": "5"}');
+
+const caneta = {
+    cor: 'azul',
+    formato: 'triangular',
+    tipo: 'desenho'
+}
+
+console.log(json);
+
+const stringjson = JSON.stringify(caneta);
+
+console.log(stringjson);

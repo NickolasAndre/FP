8c6effaa3f050dd982682e23e5f8142ff88ed69e
Nickolas Andre <nickolas.sam3@gmail.com>
2023-04-24 21:08:57 -0300

    commit

---
+++ JS021-desestruturação/js/app.js
@@ -0,0 +1,29 @@
+
+document.documentElement.addEventListener('mousemove', MouseMove);
+
+//function MouseMove(event){
+
+
+    //const{clientX, clientY} = event;
+
+    //const eixoX = event.clientX;
+    //const eixoY = event.clientY;
+
+ 
+    //console.log(clientX, clientY);
+//}
+
+//---------------------------------------
+function MouseMove({clientX, clientY}){
+
+//    console.log(clientX, clientY);
+
+}
+
+//-----------------------
+
+const carros = ['volks', 'fiat', 'bmw' ,'audi'];
+
+const [marca1, marca2, marca3, marca4] = carros;
+
+console.log(marca3)

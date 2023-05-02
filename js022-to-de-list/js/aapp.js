25b7e53ef620fd07f46cf6035f9e01d5cef1c597
Nickolas Andre <nickolas.sam3@gmail.com>
2023-05-02 20:46:16 -0300

    commit

--- js022-to-de-list/js/aapp.js
+++ js022-to-de-list/js/aapp.js
@@ -17,13 +17,39 @@ function renderTodo(){
         console.log(i)
     }
 */
-    arr.forEach((item) => {
-        var todoElement = document.createElement('li');
-        var todoText = document.createTextNode(item);
 
-        todoElement.appendChild(todoText);
-        listElement.appendChild(todoElement);
+    listElement.innerHTML = '';
+
+    arr.forEach((item, index) => {
+        var liElement = document.createElement('li');
+        var liText = document.createTextNode(item);
+
+        var linkLi = document.createElement('a');
+        var textLink = document.createTextNode('Excluir');
+
+        linkLi.setAttribute('href', '#');
+        linkLi.setAttribute('onclick', 'deleteItem('+ index+')');
+
+        linkLi.appendChild(textLink);
+
+        liElement.appendChild(liText);
+        liElement.appendChild(linkLi);
+        listElement.appendChild(liElement);
     })
 }
 
 renderTodo();
+
+buttonElement.addEventListener('click', addItem)
+
+function addItem(){
+    var itemTexto = textElement.value;
+    arr.push(itemTexto)
+    renderTodo();
+}
+
+function deleteItem(pos){
+    arr.splice(pos, 1);
+
+    renderTodo();
+}

89b75a1728da76575427cb8b1b0c8f1962298dd7
Nickolas Andre <nickolas.sam3@gmail.com>
2023-05-01 21:28:03 -0300

    commit

---
+++ js022-to-de-list/js/aapp.js
@@ -0,0 +1,29 @@
+
+var listElement = document.querySelector('.list');
+var textElement = document.querySelector('input');
+var buttonElement = document.querySelector('button');
+
+var arr = [
+    'Fazer um café',
+    'Ver aulas do FrontPUSH de JS',
+    'Tentar executar sozinho',
+];
+
+console.log(arr.length)
+
+function renderTodo(){
+/*
+    for(var i = 0; i < arr.length; i++){
+        console.log(i)
+    }
+*/
+    arr.forEach((item) => {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(item);
+
+        todoElement.appendChild(todoText);
+        todoText.appendChild(textElement);
+    })
+}
+
+renderTodo();
